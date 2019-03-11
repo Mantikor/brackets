f8f2c4b807d852d72110ef8298ed0d24b35ee382
Sergey Streltsov <sergey.streltsov@100nuts.ai>
2019-03-11 20:26:32 +0300

    without fucking stack

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    // your solution
+    // console.info(bracketsConfig);
+    // console.info(str);
+    if (str.length % 2) {
+        return false
+    } else {
+        for (let i=bracketsConfig.length; i >= 0; i--) {
+            bracketsConfig.forEach(function (entry) {
+                let br_pair = entry[0] + entry[1];
+                while (str.includes(br_pair)) {
+                    str = str.replace(br_pair, '');
+                }
+            })
+        }
+    }
+    return !Boolean(str.length)
 }
